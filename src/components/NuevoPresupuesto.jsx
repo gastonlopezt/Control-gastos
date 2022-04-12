ba6b35e0569a7042a56aed759cce41426b1f2ad6
gastonlopezt <gaston_lopeztisera@hotmail.com>
2022-04-12 15:39:12 -0300

    first commit with default css

---
+++ src/components/NuevoPresupuesto.jsx
@@ -0,0 +1,47 @@
+import {useState} from 'react'
+import Mensaje from './Mensaje'
+
+
+const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
+
+
+  const [mensaje, setMensaje] = useState('')
+
+
+
+  const handlePresupuesto = (e) => {
+    e.preventDefault();
+    
+    if(!Number(presupuesto) || Number(presupuesto) < 0){
+      setMensaje('Not a valid amount')
+    } else {
+      console.log('Valid amount')
+    }
+  }
+
+
+
+
+  return (
+    <div className='contenedor-presupuesto contenedor sombra'>
+      <form className='formulario' onSubmit={handlePresupuesto}>
+        <div className='campo'>
+          <label>Definir Presupuesto</label>
+            <input 
+              type="text" 
+              className='nuevo-presupuesto' 
+              placeholder='añade tu presupueto'
+              value={presupuesto}
+              onChange={ (e) => setPresupuesto(e.target.value) }
+            />
+        </div>
+        <input type='submit' value='Añadir' />
+
+        {mensaje && <Mensaje tipo="error" >{mensaje}</Mensaje>}
+
+      </form>
+    </div>
+  )
+}
+
+export default NuevoPresupuesto
